a05e0b5de04e6b809d2bbd6af409e9573325a164
Carlton-Ndolo <carltonmutua80@gmail.com>
2024-03-21 05:38:52 +0300

    modify index.js

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+function saturdayFun(hobby = 'roller-skate') {
+    return `This Saturday, I want to ${hobby}!`;
+}
+function mondayWork(order = 'go to the office') {
+    return `This Monday, I will ${order}.`;
+}
+function wrapAdjective(person = '*') {
+    return function(adjective = 'special') {
+        return `You are ${person}${adjective}${person}!`;
+    };
+}
+
+const praise = wrapAdjective();
+console.log(praise()); 
+
